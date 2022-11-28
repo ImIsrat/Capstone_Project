6104afa2cf9743393fd8f86b2715a2c545c41d9d
Israt Jahan <105577975+ImIsrat@users.noreply.github.com>
2022-11-28 09:10:04 +0400

    Update 2.Add Book Function.cpp

--- 2.Add Book Function.cpp
+++ 2.Add Book Function.cpp
@@ -1,14 +1,25 @@
-
-// C++ program to illustrate bookshop
-// management system using File Handling
 #include <fstream>
 #include <iostream>
+#include<conio.h>
 #include <stdio.h>
+#include <string>
 #include <stdlib.h>
 using namespace std;
 
+void control_panel();
 void add_book();
 
+void control_panel()
+{
+	system("cls");
+	cout << "\n\n\t\t\t\tCONTROL PANEL";
+	cout << "\n\n1. ADD BOOK";
+	cout << "\n2. DISPLAY BOOKS";
+	cout << "\n3. CHECK PARTICULAR BOOK";
+	cout << "\n4. UPDATE BOOK";
+	cout << "\n5. DELETE BOOK";
+	cout << "\n6. EXIT";
+}
 
 void add_book()
 {
@@ -17,29 +28,29 @@ void add_book()
 	int no_copy;
 	string b_names, a_name, b_id;
 	cout << "\n\n\t\t\t\tADD BOOKS";
-	cout << "\n\nBook ID : ";
+	cout << "\n\nBook ID(Enter only numerals) : ";
 	cin >> b_id;
 	cout << "\nBook Name : ";
-	cin >> b_names;
+	cin.ignore();
+	getline(cin,b_names);
 	cout << "\nAuthor Name : ";
-	cin >> a_name;
-	cout << "\nNo. of Books : ";
+	getline(cin,a_name);
+	cout << "\nNo. of Books(Enter only numerals) : ";
 	cin >> no_copy;
 
 	// Open file in append or
 	// output mode
-	file.open("book.txt",
+	file.open("D://book.txt",
 			ios::out | ios::app);
-	file << " " << b_id << " "
-		<< b_names << " " << a_name
-		<< " " << no_copy << "\n";
+	file<< b_id << "\n"<< b_names << "\n" << a_name<< "\n" << no_copy<<"\n";
 	file.close();
 }
 
 int main()
 {
 	// Function Call
-	add_book();
+
 
 	return 0;
 }
+
